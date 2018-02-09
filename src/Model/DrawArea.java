006fd9668b144dbd0bcd7f5e2136255eedd32d64
ConDawg <31518714+ConDawg@users.noreply.github.com>
2018-02-09 13:09:57 -0700

    Added most of my calls that are relevant to my imports.

--- src/Model/DrawArea.java
+++ src/Model/DrawArea.java
@@ -1,6 +1,16 @@
 package Model;
 
-public class DrawArea
-{
+import java.awt.Image;
+//Provides classes for creating and modifying images.
+import javax.swing.JComponent;
+import java.awt.Graphics2D;
+//Provides control over geometry, coordinate transformations, and text.
+import java.awt.event.MouseAdapter;
+//provides use of drag and moved events by the mouse.
 
-}
+public class DrawArea extends JComponent
+//JComponent provides support for tool tips, The base class for standard and custom components.
+{
+	private Image image;
+	// Picture that'll be drawn on.
+	
